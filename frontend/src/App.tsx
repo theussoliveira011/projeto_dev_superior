e72d2384fb7732c40d49065f9fecfaf4f8e76844
theussoliveira <matheusfotografia987@gmail.com>
2022-07-12 14:51:31 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return(
-    <h1>Ola mundo</h1>
+    < >
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/notificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+import './style.css'
+
+function NotificationButton() {
+    return(
+
+        <div className="dsmeta-red-btn">
+        <img src={icon} alt="icone-notificacao" />
+        </div>
+    )
+  }
+  
+  export default NotificationButton
