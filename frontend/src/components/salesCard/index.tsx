2f9b967502fdec00c6c06f9ae596ab26722b7939
theussoliveira <matheusfotografia987@gmail.com>
2022-07-15 17:05:34 -0300

    Send Notification

--- frontend/src/components/salesCard/index.tsx
+++ frontend/src/components/salesCard/index.tsx
@@ -24,13 +24,13 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
 
-        console.log(dmin);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
     }, [minDate, maxDate]);
+    
 
     return (
         <div className="card-retangulo-dsmeta">
@@ -84,7 +84,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
 
